e579111395b56e7f823ecd3bcf551a659c9d7c5d
Pollitt <hp508@exeter.ac.uk>
2022-03-16 15:47:45 +0000

    hisat2-alignment.sh

--- hisat2-alignment.sh
+++ hisat2-alignment.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --export=ALL # export all environment variables to the batch job
-#SBATCH -D . # set working directory to .
+#SBATCH -D /lustre/projects/Research_Project-T110796/nys_project/RawReads # set working directory
 #SBATCH -p pq # submit to the parallel queue
-#SBATCH --time=HH:MM:SS # maximum walltime for the job
+#SBATCH --time=08:00:00 # maximum walltime for the job
 #SBATCH -A Research_Project-XXXYYY # research project to submit under
 #SBATCH --nodes=1 # specify number of nodes
-#SBATCH --ntasks-per-node=16 # specify number of processors per node
-#SBATCH --mem=100G # specify bytes memory to reserve
+#SBATCH --ntasks-per-node=8 # specify number of processors per node
+#SBATCH --mem=30G # specify bytes memory to reserve
 #SBATCH --mail-type=END # send email at job completion
 #SBATCH --mail-user=a.n.other@exeter.ac.uk # email address
 
@@ -21,12 +21,15 @@ module load HISAT2/2.0.4-foss-2016b
 # Samtools version 1.15
 # citation https://doi.org/10.1093/bioinformatics/btp352 
 
-# align reads to reference
-destdir=/path/to/destination
+# run this script in the folder containing your fastq files
+# specify location of reference index
+REF=/lustre/projects/Research_Project-T110796/el_paco_ref/El_Paco_Index
 
-for fname in *_R1.fastq.gz
+# align reads to reference
+# loops command for each R1 file in the directory and matches it with the R2 file
+for fname in ./nys_fastqs/*_R1_001_fastp.fastq.gz
 do
     SAMPLE=${fname%_R1*}
-	hisat2 -x PpacREF -1 "${SAMPLE}_R1.fastq.gz" -2 "${SAMPLE}_R2.fastq.gz" -S "$destdir/${SAMPLE}_alignment.sam"
+	hisat2 -p 8 -x $REF --rna-strandness RF -1 "${SAMPLE}_R1_001_fastp.fastq.gz" -2 "${SAMPLE}_R2_001_fastp.fastq.gz" -S "${SAMPLE}_unsorted.sam" 2> "${SAMPLE}_unsorted_stats.txt"
 done
 
